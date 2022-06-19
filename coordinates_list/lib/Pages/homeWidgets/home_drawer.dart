4cc01aebcc58e96f108c403ef4e12b8ee8cedfec
Marcos Filipi <marcosfilipifranca@hotmail.com>
2022-06-19 16:55:50 -0300

    adicao dos botoes de login

--- coordinates_list/lib/Pages/homeWidgets/home_drawer.dart
+++ coordinates_list/lib/Pages/homeWidgets/home_drawer.dart
@@ -1,13 +1,51 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 getHomeDrawer() {
   return Drawer(
     child: ListView(
       padding: EdgeInsets.zero,
-      children: const [
+      children: [
         DrawerHeader(
-            decoration: BoxDecoration(color: Color(0xff0A6D92)),
-            child: Text("Luiz Henrique")),
+            decoration: const BoxDecoration(color: Color(0xff0A6D92)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Edit Profile",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.manage_accounts_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Log Out",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.logout_outlined,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            )),
       ],
     ),
   );
--- coordinates_list/lib/Pages/login.dart
+++ coordinates_list/lib/Pages/login.dart
@@ -1,9 +1,8 @@
-import 'dart:io';
-
 import 'package:coordinates_list/Pages/homeWidgets/home_appBar.dart';
 import 'package:coordinates_list/Pages/homeWidgets/home_drawer.dart';
 import 'package:coordinates_list/Pages/pag01.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -48,12 +47,42 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 244, 240, 250),
       appBar: getHomeAppBar(),
       drawer: getHomeDrawer(),
-      body: Container(
-        child: const Center(
-          child: Text('ola mundo!'),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              height: 40,
+              child: SignInButton(
+                elevation: 3,
+                text: "Entrar com FaceBook",
+                Buttons.FacebookNew,
+                onPressed: () {
+                  print("Funcionou");
+                },
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: SizedBox(
+              height: 40,
+              child: SignInButton(
+                elevation: 3,
+                text: "Entrar com Google",
+                Buttons.Google,
+                onPressed: () {
+                  print("Funcionou");
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
